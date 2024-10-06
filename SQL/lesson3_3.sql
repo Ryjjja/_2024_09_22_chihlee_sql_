e9b1f69b739e8904db4ee12485cf7b8b4b743c9d
Ryjjja <wznh9801@gmail.com>
2024-10-06 14:34:12 +0800

    lesson3_9

--- SQL/lesson3_3.sql
+++ SQL/lesson3_3.sql
@@ -63,3 +63,8 @@ ORDER BY code DESC; /*數字字串排序*/
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
 ORDER BY e_name; /*英文字排序*/
+
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+ORDER BY e_name  
+LIMIT 10;   /*限制前10名*/
