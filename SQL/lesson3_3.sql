3457504111955fc633f9f51e92e20ecad8f6e410
Ryjjja <wznh9801@gmail.com>
2024-10-06 11:55:01 +0800

    lesson3_4

--- SQL/lesson3_3.sql
+++ SQL/lesson3_3.sql
@@ -4,7 +4,7 @@ WHERE code = '1001'
 
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
-WHERE code = '1001' OR code = '1002'
+WHERE code = '1001' OR  code = '1002'
 
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
@@ -13,3 +13,23 @@ WHERE code IN ('1001','1002')
 SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
 FROM stations
 WHERE code = '1001' OR name = '八堵'
+
+/* 台_ 代表第一個字只要是'台'帶出來後,限制1個字數*/
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台_';
+
+/* 台% 代表第一個字只要是'台'都會帶出來,不限字數*/
+SELECT id as 序號,code as 代碼, name as 車站名稱, e_name as 英文名稱
+FROM stations
+WHERE name LIKE '台%';
+
+
+
+
+
+
+
+
+
+
