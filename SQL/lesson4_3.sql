f1deb84ab69822a938ec1441a22a0b7c365ea6f8
Ryjjja <wznh9801@gmail.com>
2024-10-13 14:37:19 +0800

    lesson4_3_4

--- SQL/lesson4_3.sql
+++ SQL/lesson4_3.sql
@@ -19,4 +19,22 @@ SELECT customer_id,
 		MIN(amount) AS 最小
 FROM payment
 GROUP BY customer_id
-ORDER BY COUNT(amount) ASC;
+ORDER BY COUNT(amount) ASC;  /**/
+
+
+SELECT customer_id,
+		SUM(amount) AS 加總,
+		AVG(amount) AS 平均數量,
+		COUNT(amount) AS 筆數,
+		MAX(amount) AS 最大,
+		MIN(amount) AS 最小
+FROM payment
+GROUP BY customer_id
+ORDER BY 筆數 ASC; /*可用中文表示*/
+
+
+/*統計寫法*/
+SELECT (first_name || ' ' || last_name) as full_name, SUM(amount) as 總合 /*字串欄位連接用|| ' ' ||表示*/
+FROM payment p JOIN customer c ON p.customer_id = c.customer_id
+GROUP BY full_name
+ORDER BY 總合 DESC;
