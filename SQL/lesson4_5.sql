bcff9a050f08f235a961f41d1ecb227dd7c5a886
Ryjjja <wznh9801@gmail.com>
2024-10-13 15:53:42 +0800

    up6

--- SQL/lesson4_5.sql
+++ SQL/lesson4_5.sql
@@ -1,8 +1,16 @@
+/*HAVING用法*/
 /*在GROUP BY之後要再搜尋，用HAVING*/
 SELECT *
 FROM payment;
 
+
 SELECT customer_id,SUM(amount) AS 總合
 FROM payment
 GROUP BY customer_id
-HAVING SUM(amount) > 200;
+HAVING SUM(amount) > 200;  /*用法*/
+
+
+SELECT store_id ,COUNT(customer_id) AS 數量
+FROM customer
+GROUP BY store_id
+HAVING COUNT(customer_id) >300;
