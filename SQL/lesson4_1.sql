995c78c2b71a85b34a287be00fdf1e66d25ec8b9
Ryjjja <wznh9801@gmail.com>
2024-10-19 12:45:00 +0800

    UPDATE

--- SQL/lesson4_1.sql
+++ SQL/lesson4_1.sql
@@ -1,10 +1,11 @@
+/*創建學生資料表*/
 CREATE TABLE students (
     student_id SERIAL PRIMARY KEY,
     name VARCHAR(10),
     age smallint
 );
 
-
+/*創建註冊表*/
 CREATE TABLE enrollments (
     enrollment_id SERIAL PRIMARY KEY,
     student_id smallint REFERENCES students(student_id),/*欄位constraint,REFERENCES*/
@@ -12,6 +13,7 @@ CREATE TABLE enrollments (
     grade VARCHAR(5)
 );
 
+/*新增學生資料*/
 INSERT INTO students (name, age) VALUES
     ('張小明', 20),
     ('李美華', 19),
@@ -19,6 +21,7 @@ INSERT INTO students (name, age) VALUES
     ('陳雅婷', 20),
     ('林志偉', 22);
 
+/*新增註冊資料 學生ID,課程,成績*/
 INSERT INTO enrollments (student_id, course_name, grade) VALUES
     (1, '資料庫系統', 'A'),
     (1, '程式設計', 'B+'),
@@ -31,7 +34,7 @@ INSERT INTO enrollments (student_id, course_name, grade) VALUES
     (5, '網頁設計', 'B+'),
     (5, '資料庫系統', 'A-');
 
-
+/*搜尋*/
 SELECT s.student_id AS 學生編號,
 			   name AS 學生姓名,
 		        age AS AGE,
