648ce9822e2b9b3e6b2490fe48ba295389dc7afa
Ryjjja <wznh9801@gmail.com>
2024-10-19 14:45:01 +0800

    UP

--- SQL/homework_1013.sql
+++ SQL/homework_1013.sql
@@ -2,11 +2,10 @@
 SELECT stationname AS 站名,
 	   SUM(gateincomingcnt) AS 進站總人數
 FROM station_in_out in_out JOIN stations s
-					ON in_out.staCode = s.stationcode
+					       ON in_out.staCode = s.stationcode
 WHERE EXTRACT(YEAR FROM date) = 2022
 GROUP BY stationname
 ORDER BY 進站總人數 DESC;
-
 
 /*全省各站點2022年進站總人數大於5佰萬人的站點*/
 SELECT stationname AS 站名,
@@ -18,7 +17,6 @@ GROUP BY stationname
 HAVING SUM(gateincomingcnt) > 5000000
 ORDER BY 進站總人數 DESC;
 
-
 /*基隆火車站2020年,每月份進站人數*/
 SELECT stationname AS 站名,
 		EXTRACT(MONTH FROM date) AS 月份,
@@ -28,7 +26,6 @@ FROM station_in_out in_out JOIN stations s
 WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname = '基隆'
 GROUP BY stationname,EXTRACT(MONTH FROM date)
 ORDER BY 月份;
-
 
 /*基隆火車站2020年,每月份進站人數,由多至少*/
 SELECT stationname AS 站名,
@@ -40,7 +37,6 @@ WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname = '基隆'
 GROUP BY stationname,EXTRACT(MONTH FROM date)
 ORDER BY 進站人數 DESC;
 
-
 /*基隆火車站2020,2021,2022,每年進站人數*/
 SELECT stationname AS 站名,
 		EXTRACT(YEAR FROM date) AS 年份,
@@ -50,7 +46,6 @@ FROM station_in_out in_out JOIN stations s
 WHERE EXTRACT(YEAR FROM date) BETWEEN 2020 AND 2022 AND stationname = '基隆'
 GROUP BY stationname, EXTRACT(YEAR FROM date)
 ORDER BY 年份;
-
 
 /*基隆火車站,臺北火車站2020,2021,2022,每年進站人數*/
 SELECT stationname AS 站名,
@@ -75,15 +70,14 @@ WHERE gateincomingcnt = (
 	    FROM station_in_out
 );
 
-
 /*各站點進站人數最多的一筆*/
 SELECT stationname AS 站名,
 	   date AS 日期,
-	   gateincomingcnt AS 進站人數
+	   gateincomingcnt AS 進站人數最多
 FROM station_in_out in_out JOIN stations s
 							ON in_out.stacode = s.stationcode
-WHERE (stacode,gateincomingcnt) IN(
+WHERE (stacode,gateincomingcnt) IN (
 		SELECT stacode, MAX(gateincomingcnt)
 		FROM station_in_out
 		GROUP BY stacode)	
-ORDER BY gateincomingcnt DESC;
+ORDER BY gateincomingcnt DESC ;
