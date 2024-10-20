5c02e4b1db695b410bb90900079c0302da2c39f8
Ryjjja <wznh9801@gmail.com>
2024-10-20 10:42:55 +0800

    1

--- SQL/RE_1013_homework.sql
+++ SQL/RE_1013_homework.sql
@@ -27,13 +27,11 @@ ORDER BY 進站總人數 DESC;
 
 
 /*基隆火車站2020年,每月份進站人數*/
-SELECT	EXTRACT(MONTH FROM date) AS 月份,
-		stationname AS 站名,
-		SUM(gateincomingcnt) AS 進站人數
-FROM station_in_out in_out JOIN stations s
-							ON in_out.stacode = s.stationcode
-WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname = '基隆'
-GROUP BY stationname, EXTRACT(MONTH FROM date);
+SELECT stationname AS 站名, TO_CHAR(date, 'YYYY-MM') AS 日期,SUM(gateincomingcnt) AS 當月總進站人數
+FROM stations JOIN station_in_out ON stationcode = stacode
+/*WHERE date BETWEEN '2022-01-01' AND '2022-12-31';*/
+WHERE EXTRACT(YEAR FROM date) = 2020 AND stationname='基隆'
+GROUP BY 站名,日期;
 
 
 /*基隆火車站2020年,每月份進站人數,由多至少*/
