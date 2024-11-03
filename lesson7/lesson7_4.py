62716fd59f3818559ea42040902780c5d9eeca83
Ryjjja <wznh9801@gmail.com>
2024-11-03 16:05:36 +0800

    up

--- lesson7/lesson7_4.py
+++ lesson7/lesson7_4.py
@@ -19,7 +19,13 @@ conn.close()
 st.title('世界大盤分析')
 with st.sidebar:
     st.sidebar.title('請選擇股票市場:')
-    st.write('## 台灣')
-    st.write(all_country)
+    input_dict= dict(all_country)
+    options= st.multiselect("請選擇",input_dict.values(),default='台灣',placeholder='請選擇市場',label_visibility='hidden')
+    st.write(options)
+
+
+
+
+
 
 ## 執行時，先進入chihlee_sql的虛擬環境 / lesson7的整合終端機 / 輸入 streamlit run lesson7_4.py
