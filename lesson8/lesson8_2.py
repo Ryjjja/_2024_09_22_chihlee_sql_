9528f236fba228179d2cf2b70d3ac1c11df4b0ae
Ryjjja <wznh9801@gmail.com>
2024-11-10 10:56:32 +0800

    up

--- lesson8/lesson8_2.py
+++ lesson8/lesson8_2.py
@@ -17,16 +17,18 @@ conn.close()
 
 def user_select():
     print('使用者選擇了') #定義funtion
-    print(options)
+    print(st.session_state.stocks)
 
 st.title('世界大盤分析')
 with st.sidebar:
+    default_country = '台灣'
     st.sidebar.title('請選擇股票市場:')
     input_dict= dict(all_country)
-    options= st.multiselect("請選擇",input_dict.values(),
-                            default='台灣',
-                            placeholder='請選擇市場',
-                            label_visibility='hidden',
-                            on_change=user_select) #funtions名稱
-    print(type(options))
-    st.write(options)
+    st.multiselect("請選擇",input_dict.values(),   #若不需要一開始傳出值,開頭options= 就拿掉,縮排往前
+                    default=default_country,
+                    placeholder='請選擇市場',
+                    label_visibility='hidden',
+                    key='stocks',  #key
+                    on_change=user_select) #funtions名稱
+    st.write(default_country)
+  
